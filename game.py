2295620737bcc57c2aca3e010e8e3c69a02bb52d
Noah Roufus <nroufus@infowest.com>
2020-11-13 10:38:09 -0700

    Added Object class, and supporting code to loop.

--- game.py
+++ game.py
@@ -4,18 +4,27 @@ class game:
     screenSize = screenWidth, screenHeight = 320, 240
     screen = 0
     running = False
+    objects = []
 
     def __init__(self):
-        self.screen = pygame.display.set_mode(self.screenSize)
         pygame.init()
+        self.screen = pygame.display.set_mode(self.screenSize)
+        
 
     def update(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+        
+        for obj in self.objects:
+            obj.update()
     
     def render(self):
         self.screen.fill((0,0,0))
+
+        for obj in self.objects:
+            obj.render()
+
         pygame.display.flip()
 
     def loop(self):
