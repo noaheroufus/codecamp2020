001c80a27253d46a8e65cce665dd012420a73494
Noah Roufus <nroufus@infowest.com>
2020-11-13 17:07:31 -0700

    Added Inventory and InventoryItem base class

--- player.py
+++ player.py
@@ -1,15 +1,24 @@
 from object import Object
 from event import Event
+from inventory import Inventory
 
 class Player(Object):
+    inventory = False
+
     def __init__(self, game, position, size, graphic=False):
         super().__init__(game, position, size, graphic)
+
+        self.inventory = Inventory(100)
 
     def update(self):
         super().update()
 
     def render(self):
         super().render()
+
+        item = self.inventory.get_active_item()
+        if item:
+            item.render(self.game, self.position)
 
     def handle_event(self, event):
         if event.type == Event.EVENT_PLAYER_MOVE_RIGHT:
