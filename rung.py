0d8fdaf28d3eb71e03ecdab2946a6f1f9612823b
Stetofire <thekeithrocks@gmail.com>
2020-11-13 20:44:50 -0700

    Weighted Randomly choose rungs

--- rung.py
+++ rung.py
@@ -2,5 +2,20 @@ from object import Object
 from graphic import Graphic
 
 class Rung(Object):
+    def __init__(self, game, position, graphic=False):
+        g = graphic
+        if not g:
+            g = Graphic([game.graphics.rung],[0])
+        super().__init__(game, position, graphic=g)
+
+class RungFrozen(Rung):
     def __init__(self, game, position):
-        super().__init__(game, position, graphic=Graphic([game.graphics.rung],[0]))
+        super().__init__(game, position, Graphic([game.graphics.rung_frozen],[0]))
+
+class RungPacket(Rung):
+    def __init__(self, game, position):
+        super().__init__(game, position, Graphic([game.graphics.rung_packet],[0]))
+
+class RungUpgradeStation(Rung):
+    def __init__(self, game, position):
+        super().__init__(game, position, Graphic([game.graphics.rung_upgrade_station],[0]))
