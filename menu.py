75372dbddf794ffd8b06d8c835627fe7f9d72e70
Stetofire <thekeithrocks@gmail.com>
2020-11-14 00:00:32 -0700

    Make Menu as big as there are options

--- menu.py
+++ menu.py
@@ -3,11 +3,15 @@ import pygame
 
 # Stores a background image and is able to switch between multiple options and select one
 class Menu(Object):
-    def __init__(self, game, position, options, size=(64,32), background=False, pointer=False):
-        super().__init__(game, position, size, background)
+    def __init__(self, game, position, options, pointer=False):
+        super().__init__(game, position)
         self.options = options
         self.selection = 0
         self.pointer = pointer
+        self.height = int(len(self.options)/2)
+        if self.height < len(self.options)/2:
+            self.height += 1
+        self.width = 3
 
     def handle_event(self, event):
         if event.key == pygame.K_DOWN:
@@ -16,6 +20,17 @@ class Menu(Object):
                 self.selection = len(self.options)-1
 
     def render(self):
-        super().render()
+        for i in range(self.height):
+            for j in range(self.width):
+                cell = self.game.graphics.menu_c
+                if i == 0 and j == 0: cell = self.game.graphics.menu_sw
+                elif i == 0 and j > 0 and j < self.width-1: cell = self.game.graphics.menu_s
+                elif i == 0 and j == self.width-1: cell = self.game.graphics.menu_se
+                elif i > 0 and i < self.height-1 and j == 0: cell = self.game.graphics.menu_w
+                elif i == self.height-1 and j == 0: cell = self.game.graphics.menu_nw
+                elif i == self.height-1 and j > 0 and j < self.width-1: cell = self.game.graphics.menu_n
+                elif i == self.height-1 and j == self.width-1: cell = self.game.graphics.menu_ne
+                elif i > 0 and i < self.height-1 and j == self.width-1: cell = self.game.graphics.menu_e
+                self.game.canvas.surface.blit(cell, (self.position[0]+(self.game.sprite_width*j), self.position[1]-(self.game.sprite_height*(i+1))))
         if self.pointer:
             pass
