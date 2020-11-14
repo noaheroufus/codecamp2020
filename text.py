cc5b94e92f703c05bc1bced00be3e6be3414dd9e
Noah Roufus <nroufus@infowest.com>
2020-11-14 03:23:50 -0700

    Loading font from file, added size/color.

--- text.py
+++ text.py
@@ -1,11 +1,14 @@
 from object import Object
-import pygame
+import pygame,os
 
 class Text(Object):
-    def __init__(self, game, position, text):
+    def __init__(self, game, position, text, size=False, color=(0,0,0)):
         super().__init__(game, position, (5,5))
-        self.font = pygame.font.SysFont("Bungee Inline", int(game.sprite_height/2-(game.sprite_height/10)))
+        if not size:
+            size = int(game.sprite_height/2-(game.sprite_height/10))
+        self.font = pygame.font.Font(os.path.join('fonts', 'BungeeInline.ttf'), size)
         self.text = text
+        self.color = color
 
     def render(self):
-        self.game.canvas.surface.blit(self.font.render(self.text, False, (0,0,0)), self.position)
+        self.game.canvas.surface.blit(self.font.render(self.text, False, self.color), self.position)
